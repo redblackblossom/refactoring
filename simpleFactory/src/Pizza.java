517ee9571cb2e1892b05e0f8c941139f4ff4da58
redblackblossom <155342235+redblackblossom@users.noreply.github.com>
2024-03-30 12:34:44 +0900

    심플 팩토리 코드

---
+++ simpleFactory/src/Pizza.java
@@ -0,0 +1,11 @@
+public class Pizza {
+    public void prepare(){}
+    public void bake(){}
+    public void cut(){}
+    public void box(){}
+}
+
+class CheesePizza extends Pizza{}
+class GreekPizza extends Pizza{}
+class PepperoniPizza extends Pizza{}
+
